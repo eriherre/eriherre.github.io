300a8fddc41ce92383a4e5a9aeca631f4dc06861
Gonzalo Suarez <gonzalo.suarez.v@gmail.com>
2024-10-17 16:40:59 +1300

    Animate get resume button

--- docs/static/jscript/common.js
+++ docs/static/jscript/common.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   highLightMenu();
+  footerData();
 });
 
 function highLightMenu() {
   var path = window.location.pathname;
-  var page = path.split("/")[1] || "index"; 
+  var page = path.split("/")[1] || "index";
   var menu = document.getElementById("menu-" + page);
 
   if (menu) {
@@ -12,4 +13,10 @@ function highLightMenu() {
   } else {
     console.warn("Menu item not found for page:", page);
   }
+}
+
+function footerData() {
+  const currentYear = new Date().getFullYear();
+  const footerText = `\u00A9 ${currentYear} Erika Herrera \u2014 Learning Designer`;
+  document.getElementById("footerDescription").textContent = footerText;
 }
